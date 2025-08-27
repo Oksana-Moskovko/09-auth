7fd928218f8817eb53ca6ab2253498021188fafc
Oksana <omoskovko98@gmail.com>
2025-08-27 16:46:50 +0300

    перейменувала папку

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -1,10 +1,6 @@
 import axios from 'axios'
 
 export const nextServer = axios.create({
-  // baseURL: 'http://localhost:3000/api',
   baseURL: process.env.NEXT_PUBLIC_API_URL + "/api",
   withCredentials: true,
 })
-
-// getNotes = tags
-// export const getNotes = ["Work", "Todo", "Personal", "Meeting", "Shopping"];
